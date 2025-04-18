c46e9c64f980efc1265541d8b0e214295788ba92
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-18 18:16:19 +0200

    robot test

---
+++ src/test/java/resourcesd/RobotTest.java
@@ -0,0 +1,75 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import za.co.wethinkcode.toyrobot.maze.EmptyMaze;
+import za.co.wethinkcode.toyrobot.world.IWorld;
+import za.co.wethinkcode.toyrobot.world.TextWorld;
+import za.co.wethinkcode.toyrobot.world.IWorld.Direction;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+class RobotTest {
+    @Test
+    void initialPosition() {
+        Robot robot = new Robot("CrashTestDummy", new TextWorld(new EmptyMaze()));
+        assertEquals(IWorld.CENTRE, robot.getPosition());
+        assertEquals(Direction.UP, robot.getCurrentDirection());
+    }
+
+    @Test
+    void dump() {
+        Robot robot = new Robot("CrashTestDummy", new TextWorld(new EmptyMaze()));
+        assertEquals("[0,0] CrashTestDummy> Ready", robot.toString());
+    }
+
+    @Test
+    void shutdown() {
+        Robot robot = new Robot("CrashTestDummy", new TextWorld(new EmptyMaze()));
+        ShutdownCommand command = new ShutdownCommand();
+        assertFalse(robot.handleCommand(command));
+        assertEquals(IWorld.UpdateResponse.SHUTDOWN, robot.getStatus());
+    }
+
+    @Test
+    void forward() {
+        Robot robot = new Robot("CrashTestDummy", new TextWorld(new EmptyMaze()));
+        ForwardCommand command = new ForwardCommand("10");
+        assertTrue(robot.handleCommand(command));
+        Position expectedPosition = new Position(IWorld.CENTRE.getX(), IWorld.CENTRE.getY() + 10);
+        assertEquals(expectedPosition, robot.getPosition());
+        assertEquals(IWorld.UpdateResponse.SUCCESS, robot.getStatus());
+        assertEquals("Moved forward by 10 steps.", robot.getStatus().getMessage());
+    }
+
+    @Test
+    void forwardforward() {
+        Robot robot = new Robot("CrashTestDummy", new TextWorld(new EmptyMaze()));
+        assertTrue(robot.handleCommand(new ForwardCommand("10")));
+        assertTrue(robot.handleCommand(new ForwardCommand("5")));
+        assertEquals(IWorld.UpdateResponse.SUCCESS, robot.getStatus());
+        assertEquals("Moved forward by 5 steps.", robot.getStatus().getMessage());
+    }
+
+    @Test
+    void tooFarForward() {
+        Robot robot = new Robot("CrashTestDummy", new TextWorld(new EmptyMaze()));
+        assertTrue(robot.handleCommand(new ForwardCommand("1000")));
+        assertEquals(IWorld.CENTRE, robot.getPosition());
+        assertEquals(IWorld.UpdateResponse.FAILED_OUTSIDE_WORLD, robot.getStatus());
+        assertEquals("Sorry, I cannot go outside my safe zone.", robot.getStatus().getMessage());
+    }
+
+    @Test
+    void help() {
+        Robot robot = new Robot("CrashTestDummy", new TextWorld(new EmptyMaze()));
+        Command command = new HelpCommand();
+        assertTrue(robot.handleCommand(command));
+        assertEquals(IWorld.UpdateResponse.HELP, robot.getStatus());
+        assertEquals(IWorld.UpdateResponse.HELP.getMessage(), robot.getStatus().getMessage());
+    }
+}
